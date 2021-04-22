ab0b27cfd23c28bec096cff41de786c7e67ba449
CJParry <chris.parry@windowslive.com>
2021-04-22 16:34:49 +1200

    Added camera component to AddContent page

--- diary/pages/AddContent.js
+++ diary/pages/AddContent.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { SafeAreaView, Text } from 'react-native';
+import Camera from './Camera';
+
 
 // This page allows the user to take a picture with their camera, or upload from their phone memory
 
@@ -7,6 +9,9 @@ const AddContent = () => {
   return (
     <SafeAreaView>
         <Text>Page to take pictures and upload content, upload here!</Text>
+    <Camera>
+      
+    </Camera>
     </SafeAreaView>
   );
 };
